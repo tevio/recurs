ea3e2f11e913ddf0d4dc7f2ea6b288b16ac94e39
tevio <steve@shakewell.co.uk>
2010-12-16 03:07:50 +0000

    testing view generation

--- lib/generators/recurs_widget_generator.rb
+++ lib/generators/recurs_widget_generator.rb
@@ -1,6 +1,6 @@
 #module
 require 'rails/generators/migration'
-require 'actic'
+require 'actic/lib/generators/actic_generator'
 class RecursWidgetGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
   include Actic::Generator
